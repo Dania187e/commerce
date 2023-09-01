87997564954dbde8c7ac7e070640cd341785db79
Dania187e <dania.abdelhalim@uoitc.edu.iq>
2023-09-01 21:48:41 +0300

    all files

--- auctions/models.py
+++ auctions/models.py
@@ -15,29 +15,26 @@ class Category(models.Model):
     
 
 class Bid(models.Model):
-    bid=models.FloatField(default=0)
+    bid=models.IntegerField()
     user= models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="userBid")
 
-    def __str__(self):
-        return f"Bid by {self.user.username}: {self.bid}"
     
 
 class Listing(models.Model):
     title=models.CharField(max_length=30)
     description= models.CharField(max_length=300)
     imageUrl= models.CharField(max_length=1000)
-    price=models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, null=True, related_name="bid_price")
+    price=models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, null=True, related_name="bidprice")
     isActive=models.BooleanField(default=True)
     owner=models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
-    category=models.ForeignKey(Category,on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
     watchlist=models.ManyToManyField(User, blank=True, null=True, related_name="listingWatchlist")
 
     def __str__(self):
-       
-       return self.title
+        return self.title
 
 class Comment(models.Model):
-    author=models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True,  related_name="usercomment")
+    author=models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True,  related_name="userComment")
     listing=models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, null=True,  related_name="listingcomment")
     message=models.CharField(max_length=200)
 
--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,63 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse
 
 from .models import User, Category, Listing, Comment, Bid
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
+
 
 def listing(request, id):
     listingData=Listing.objects.get(pk=id)
@@ -13,41 +66,56 @@ def listing(request, id):
     return render(request, "auctions/listing.html",{
         "listing":listingData,
         "isListingInWatchlist": isListingInWatchlist,
-        "allComments": allComments
+        "allComments": allComments,
+       
+            
+    })
 
-    } )
 
-
+def closeAuction(request, id):
+    listingData=Listing.objects.get(pk=id)
+    listingData.isActive=False
+    isListingInWatchlist=request.user in listingData.watchlist.all()
+    allComments=Comment.objects.filter(listing=listingData)
+    listingData.save()
+    return render(request, "auctions/listing.html",{
+        "listing":listingData,
+        "isListingInWatchlist": isListingInWatchlist,
+        "allComments": allComments,
+        "updated": True,
+        "message":  "congrats! the Auction is closed"
+    })
 
 def addBid(request, id):
-    if request.method =="POST":
-       newBid= int(request.POST.get("newBid"))
-       listingData=Listing.objects.get(pk=id)
-
-       highest_bid=listingData.price.bid
-       
-       if newBid > highest_bid:
-           updatedBid=Bid(user=request.user, bid=newBid)
-           updatedBid.save()
-
-           listingData.price=updatedBid
-           listingData.save()
-
-           message= "Bid was updated successfuly"
-           updated= True 
-       else:
-            message="Bid updated Failed "
-            updated=False
-
-       return render(request, "auctions/listing.html",{
+    newBid=request.POST["newBid"]
+    listingData=Listing.objects.get(pk=id)
+    isListingInWatchlist=request.user in listingData.watchlist.all()
+    allComments=Comment.objects.filter(listing=listingData) 
+ 
+    if int(newBid) > listingData.price.bid:
+        updatedBid=Bid(user=request.user, bid=int(newBid))
+        updatedBid.save()
+        listingData.price=updatedBid
+        listingData.save()
+        return render(request, "auctions/listing.html",{
             "listing": listingData,
-            "message": message ,
-            "updated": updated
-
-        })
+            "message":  "Bid was updated successfully",
+            "updated": True,
+            "isListingInWatchlist": isListingInWatchlist,
+            "allComments": allComments,
+            
+         })
     else:
-       return HttpResponseRedirect(reverse("index"))
-
+        return render(request, "auctions/listing.html",{
+            "listing": listingData,
+            "message":  "Bid updated Failed.",
+            "updated": False,
+            "isListingInWatchlist": isListingInWatchlist,
+            "allComments": allComments,
+            
+        })
+    
+    
 def addComment(request, id):
     currentUser=request.user
     listingData=Listing.objects.get(pk=id)
@@ -165,53 +233,3 @@ def CreateListing(request):
 
 
 
-def login_view(request):
-    if request.method == "POST":
-
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "auctions/login.html")
-
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
-
-
-def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
-
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "auctions/register.html")
